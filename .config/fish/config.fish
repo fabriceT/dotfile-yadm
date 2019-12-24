1973f99b14b513800bf92f282443580c77f49e82
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2019-12-24 16:00:31 +0100

    clean up in abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
 abbr -a gco git checkout
 abbr -a osquerya osqueryi  --nodisable_audit --nodisable_events --audit_allow_config=true --audit_persist=true --audit_allow_sockets --logger_plugin=filesystem --events_expiry=1
-abbr -a ddrop /usr/bin/dotdrop -c ~/Projets/dotdrop/config.yaml
+
+set -gx PATH /home/phab/go/bin $PATH
