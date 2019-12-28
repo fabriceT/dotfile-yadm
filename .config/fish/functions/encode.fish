948dd136ef90bff6dc8b4220db25b2b7ba4d2436
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2019-12-28 17:24:55 +0100

    modify encode function to take arguments

--- .config/fish/functions/encode.fish
+++ .config/fish/functions/encode.fish
@@ -1,7 +1,16 @@
-function encode 
-  set files (find . -maxdepth 2 -name "*.mp4")
+function encode --description "Encode video files to x265 mp4"
+  set -l pattern "*.mp4"
+
+  # if an argument passed, modify $pattern
+  if test (count $argv) -gt 0
+	echo "setting pattern to $argv[1]"
+	set pattern $argv[1]
+  end
+
+  echo "Listing files $pattern in current directory"
+  set -l files (find . -maxdepth 2 -name $pattern)
   if test (count $files) -gt 1
-    mkdir new
+    mkdir new 2>/dev/null
     for file in $files
       set dir (dirname $file)
       mkdir -p new/$dir
