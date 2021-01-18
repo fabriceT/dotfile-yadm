46319652e85de36fb1b8c93c6e8a1a3c752be84d
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2021-01-18 21:21:49 +0100

    Add wayland backend for firefox

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,4 @@ abbr -a gco git checkout
 abbr -a osquerya osqueryi  --nodisable_audit --nodisable_events --audit_allow_config=true --audit_persist=true --audit_allow_sockets --logger_plugin=filesystem --events_expiry=1
 
 set -gx PATH /home/phab/go/bin $PATH
+set -gx MOZ_ENABLE_WAYLAND 1
