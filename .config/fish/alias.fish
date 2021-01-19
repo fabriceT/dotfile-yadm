e6be5c93ebbc740e5d0b3d2d13d16063a8791a1e
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2021-01-19 11:01:55 +0100

    Fish: more aliases

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,17 +1,32 @@
-
-
-abbr -a gco git checkout
-abbr -a gcm git commit -m 
+# GIT
+abbr -a gck git checkout
+abbr -a gco git commit -m 
 abbr -a gst git status
 abbr -a gsw git switch
-abbr -a gpul git pull
-abbr -a gpus git push
+abbr -a gpl git pull
+abbr -a gpu git push
 
-abbr -a yco yadm checkout
-abbr -a ycm yadm commit -m 
+# YADM
+abbr -a yck yadm checkout
+abbr -a yco yadm commit -m 
 abbr -a yst yadm status
-abbr -a ypul yadm pull
-abbr -a ypus yadm push
+abbr -a ypl yadm pull
+abbr -a yph yadm push
+abbr -a ybp yadm bootstrap
+abbr -a yec yadm encrypt
+abbr -a ydc yadm decrypt
+
+# Just because...
+abbr -a docker podman
+abbr -a docker-compose podman-compose
+
+# Systemd
+abbr -a sysreload  systemctl reload
+abbr -a sysdreload systemctl deamon-reload
+abbr -a sysstop    systemctl stop
+abbr -a sysstart   systemctl start
+abbr -a systimers  systemctl list-timers
+
 
 abbr -a osquerya osqueryi  --nodisable_audit --nodisable_events --audit_allow_config=true --audit_persist=true --audit_allow_sockets --logger_plugin=filesystem --events_expiry=1
 
