24a33af2d3257b0c677db9d7feb7d04f43cbc43a
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2021-09-15 20:37:45 +0200

    Various update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,4 @@
 source ~/.config/fish/alias.fish
 
-abbr -a gco git checkout
-abbr -a osquerya osqueryi  --nodisable_audit --nodisable_events --audit_allow_config=true --audit_persist=true --audit_allow_sockets --logger_plugin=filesystem --events_expiry=1
-
 set -gx PATH ~/go/bin ~/.local/bin $PATH
 set -gx MOZ_ENABLE_WAYLAND 1
--- .config/fish/functions/fish_prompt.fish
+++
@@ -1,1 +0,0 @@
-/home/phab/.local/share/omf/themes/shellder/fish_prompt.fish
