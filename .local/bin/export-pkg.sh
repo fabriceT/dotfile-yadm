05f3757d1731974f855bd63d22ef503986610e85
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2022-07-16 21:34:34 +0200

    packages: add script to export installed packages (pacman + stew)

---
+++ .local/bin/export-pkg.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+EXPORT_FILE=~/.local/var/packages.lst##$(uname -n)
+echo ${EXPORT_FILE}
+
+echo "[pacman-explicit]" > ${EXPORT_FILE}
+pacman -Qte |  awk '{ print $1 }' >> ${EXPORT_FILE}
+
+echo -e "\n[pacman-deps]" >> ${EXPORT_FILE}
+pacman -Qtd |  awk '{ print $1 }' >> ${EXPORT_FILE}
+
+echo -e "\n[pacman-deps]" >> ${EXPORT_FILE}
+stew ls '{ print $1 }' >> ${EXPORT_FILE}
