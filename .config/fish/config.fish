3889a457b17282dc6bdf4b7355ed92034318b3f7
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2022-08-12 13:59:45 +0200

    fish: update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,4 @@ set -gx MOZ_ENABLE_WAYLAND 1
 set -gx EDITOR micro
 
 # Fix act https://github.com/nektos/act/issues/303
-# No need if .actrc contains
-#   --bind --container-daemon-socket /run/user/1000/podman/podman.sock
-#set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
+set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
