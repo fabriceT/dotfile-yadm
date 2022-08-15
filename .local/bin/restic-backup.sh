00e445696adc9536cbbda4f3667299cac6b039ae
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2022-08-15 16:20:35 +0200

    scripts: add restic backup script

---
+++ .local/bin/restic-backup.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+
+DISK_REPO="/run/media/phab/1ea5cfde-2f7d-4b3f-b4c0-417473d14af1/backup/"
+SFTP_REPO="sftp:thfa9713@www.kill-swit.ch:/home/thfa9713/restic-repo/"
+
+RESTIC_PASSWORD=$(secret-tool lookup application restic)
+export RESTIC_PASSWORD
+
+function backup() {
+    export RESTIC_REPOSITORY=$1
+
+    restic forget --keep-monthly 1 --keep-yearly 1 --prune
+
+    restic backup --files-from=/home/phab/.config/restic.include \
+                  --exclude-file=/home/phab/.config/restic.exclude
+}
+
+
+if [[ -d ${DISK_REPO} ]]; then
+    backup ${DISK_REPO}
+fi
+
+#backup ${SFTP_REPO}
