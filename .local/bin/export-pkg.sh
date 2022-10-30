890fa781d8dc6e6f31a73211a7bf7c273db38f0f
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2022-10-30 10:22:38 +0100

    Packages(fix): make updates great again

--- .local/bin/export-pkg.sh
+++ .local/bin/export-pkg.sh
@@ -9,5 +9,5 @@ pacman -Qte | awk '{ print $1 }' >> "${EXPORT_FILE}"
 echo -e "\n[pacman-deps]" >> "${EXPORT_FILE}"
 pacman -Qtd | awk '{ print $1 }' >> "${EXPORT_FILE}"
 
-echo -e "\n[pacman-deps]" >> "${EXPORT_FILE}"
+echo -e "\n[stew]" >> "${EXPORT_FILE}"
 stew ls | awk '{ print $1 }' >> "${EXPORT_FILE}"
