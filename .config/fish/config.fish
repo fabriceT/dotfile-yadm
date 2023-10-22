52062b067e089c361622e5297bf4d62ede43c829
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2023-10-22 18:32:04 +0200

    fix(shell): add aqua configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
-set -gx PATH ~/go/bin ~/.local/bin $PATH
+set -gx PATH ~/go/bin ~/.local/bin ~/.local/share/aquaproj-aqua/bin $PATH
 set -gx MOZ_ENABLE_WAYLAND 1
 set -gx EDITOR micro
+
+set -gx AQUA_GLOBAL_CONFIG ~/.config/aquaproj-aqua/aqua.yaml
 
 # Python virtual env won't override my prompt
 set -gx VIRTUAL_ENV_DISABLE_PROMPT "I'm not empty"
@@ -10,8 +12,3 @@ oh-my-posh init fish --config /usr/share/oh-my-posh/themes/peru.omp.json | sourc
 set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
 
 source ~/.config/fish/alias.fish
-
-#oh-my-posh init fish --config /usr/share/oh-my-posh/themes/quick-term.omp.json | source
-oh-my-posh init fish --config /usr/share/oh-my-posh/themes/peru.omp.json | source
-#oh-my-poshd init fish --config ~/.config/oh-my-posh/quick-term.omp.json | source
-thefuck --alias | source
