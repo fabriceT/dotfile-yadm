bcc5b68a2383bb7045b2d8fc4946eede6d83fa70
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2023-10-22 18:30:32 +0200

    fix(backup): update backup period

--- .local/bin/restic-backup.sh
+++ .local/bin/restic-backup.sh
@@ -1,17 +1,17 @@
 #!/bin/env bash
 
 declare -A REPOSITORIES=(
-  [disk]="/run/media/phab/1ea5cfde-2f7d-4b3f-b4c0-417473d14af1/backup/"
-#  [remote]="sftp:thfa9713@www.kill-swit.ch:/home/thfa9713/restic-repo/" 
+  [disk]="/run/media/${USER}/1ea5cfde-2f7d-4b3f-b4c0-417473d14af1/backup/"
+  [remote]="sftp:thfa9713@www.kill-swit.ch:/home/thfa9713/restic-repo/" 
 )
 
 function backup() {
     restic backup "${HOME}" --exclude-file="${HOME}/.config/restic.exclude"
-    restic forget --keep-weekly 4 --keep-monthly 1 --keep-yearly 1 --prune
+    restic forget --keep-weekly 1m --keep-monthly 1y --keep-yearly 2y --prune
 }
 
 for location in ${!REPOSITORIES[@]}; do
-echo $location
+	echo $location
 	export RESTIC_REPOSITORY=${REPOSITORIES[$location]}
 	export RESTIC_PASSWORD=$(secret-tool lookup application restic)
 	backup
