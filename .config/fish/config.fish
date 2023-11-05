652c0cc27e5a72f51c3d0b60c6affa27299c3e80
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2023-11-05 18:17:44 +0100

    chores(shell): update environment variables for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,15 @@
-set -gx PATH ~/go/bin ~/.local/bin ~/.local/share/aquaproj-aqua/bin $PATH
+set -gx PATH $PATH ~/go/bin ~/.local/bin ~/.local/share/aquaproj-aqua/bin
 set -gx MOZ_ENABLE_WAYLAND 1
 set -gx EDITOR micro
-
+set -gx CHARM_HOST 130.61.241.116
 set -gx AQUA_GLOBAL_CONFIG ~/.config/aquaproj-aqua/aqua.yaml
 
 # Python virtual env won't override my prompt
 set -gx VIRTUAL_ENV_DISABLE_PROMPT "I'm not empty"
-oh-my-posh init fish --config /usr/share/oh-my-posh/themes/peru.omp.json | source
 
 # Fix act https://github.com/nektos/act/issues/303
 set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
 
 source ~/.config/fish/alias.fish
+
+oh-my-posh init fish --config /usr/share/oh-my-posh/themes/peru.omp.json | source
