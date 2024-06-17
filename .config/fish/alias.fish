4162db5acf274d7f5f6bed1eccd142fd9685f689
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2024-06-17 08:45:27 +0200

    fix(fish): can't remember why? :D Minor changes

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,4 +1,6 @@
-# GIT
+# aliases.fish
+
+## GIT
 abbr -a gick git checkout
 abbr -a gico git commit -m
 abbr -a gist git status
@@ -6,7 +8,7 @@ abbr -a gisw git switch
 abbr -a gipl git pull
 abbr -a giph git push
 
-# YADM
+## YADM
 abbr -a yadck yadm checkout
 abbr -a yadco yadm commit -m
 abbr -a yadst yadm status
@@ -16,11 +18,11 @@ abbr -a yadbp yadm bootstrap
 abbr -a yadenc yadm encrypt
 abbr -a yaddec yadm decrypt
 
-# Just because...
+## Just because...
 # abbr -a docker podman
 # abbr -a docker-compose podman-compose
 
-# Systemd
+## Systemd
 abbr -a sysreload systemctl reload
 abbr -a sysdreload systemctl deamon-reload
 abbr -a sysstop systemctl stop
@@ -28,7 +30,14 @@ abbr -a sysstart systemctl start
 abbr -a systimers systemctl list-timers
 abbr -a sysreboot systemctl reboot
 
+## terraform
+abbr -a tfi terraform init
+abbr -a tfp terraform plan -out=terraform.tfstate.plan
+abbr -a tfo terraform output
+abbr -a tfa terraform apply terraform.tfstate.plan
+
+## Misc
 abbr -a osquerya osqueryi --nodisable_audit --nodisable_events --audit_allow_config=true --audit_persist=true --audit_allow_sockets --logger_plugin=filesystem --events_expiry=1
 
 abbr -a act act --bind --container-daemon-socket {$XDG_RUNTIME_DIR}/podman/podman.sock
-abbr -a torrent 'mtorrent -d ~/Téléchargements/P2P --torrent-dir ~/Téléchargements/ -w ~/Téléchargements/ --magnet-file ~/Téléchargements/magnet.links'
+abbr -a torrent 'mtorrent -d ~/Téléchargements/P2P --torrent-dir ~/Téléchargements/ -w ~/Téléchargements/ --magnet-file ~/magnets.link'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
 set -gx PATH $PATH ~/go/bin ~/.local/bin ~/.local/share/aquaproj-aqua/bin
+set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
 set -gx MOZ_ENABLE_WAYLAND 1
 set -gx EDITOR micro
 set -gx CHARM_HOST 130.61.241.116
-set -gx AQUA_GLOBAL_CONFIG ~/.config/aquaproj-aqua/aqua.yaml
+set -gx AQUA_GLOBAL_CONFIG $HOME/.config/aquaproj-aqua/aqua.yaml
 
 # Python virtual env won't override my prompt
 set -gx VIRTUAL_ENV_DISABLE_PROMPT "I'm not empty"
