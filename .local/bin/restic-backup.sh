96652b35aab22557df4a9a64ec08a5a865f0aea3
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2024-06-17 08:43:13 +0200

    fix(scripts): prune backups for current host only

--- .local/bin/restic-backup.sh
+++ .local/bin/restic-backup.sh
@@ -2,12 +2,12 @@
 
 declare -A REPOSITORIES=(
   [disk]="/run/media/${USER}/1ea5cfde-2f7d-4b3f-b4c0-417473d14af1/backup/"
-  [remote]="sftp:thfa9713@www.kill-swit.ch:/home/thfa9713/restic-repo/" 
+#  [remote]="sftp:thfa9713@www.kill-swit.ch:/home/thfa9713/restic-repo/" 
 )
 
 function backup() {
     restic backup "${HOME}" --exclude-file="${HOME}/.config/restic.exclude"
-    restic forget --keep-weekly 5 --keep-monthly 12 --keep-yearly 2 --prune
+    restic forget --keep-weekly 5 --keep-monthly 12 --keep-yearly 2 --prune --host $(hostname)
 }
 
 for location in ${!REPOSITORIES[@]}; do
@@ -16,4 +16,3 @@ for location in ${!REPOSITORIES[@]}; do
 	export RESTIC_PASSWORD=$(secret-tool lookup application restic)
 	backup
 done
-
