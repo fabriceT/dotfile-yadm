8544ab06ed5315d0bcdd1c9fcf37101c7686f922
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2024-10-19 06:22:00 +0000

    Wip/ci (#4)
    * fix
    
    * feat: add CI
    
    * fix(fish): nothing harmed here
    
    * fix(bookmarks): cleanup
    
    * fix(scripts): fix lint

--- .local/bin/export-pkg.sh
+++ .local/bin/export-pkg.sh
@@ -4,6 +4,7 @@ EXPORT_FILE=~/Sync/packages-$(uname -n).txt
 echo "${EXPORT_FILE}"
 
 echo "[pacman-explicit]" > "${EXPORT_FILE}"
+# shellcheck disable=SC2129
 pacman -Qte | awk '{ print $1 }' >> "${EXPORT_FILE}"
 
 echo -e "\n[pacman-deps]" >> "${EXPORT_FILE}"
@@ -13,7 +14,7 @@ echo -e "\n[stew]" >> "${EXPORT_FILE}"
 stew ls --tags >> "${EXPORT_FILE}"
 
 echo -e "\n[fisher]" >> "${EXPORT_FILE}"
-sort $HOME/.config/fish/fish_plugins >> "${EXPORT_FILE}"
+sort "$HOME/.config/fish/fish_plugins" >> "${EXPORT_FILE}"
 
 echo -e "\n[gox]" >> "${EXPORT_FILE}"
 gox list >> "${EXPORT_FILE}"
