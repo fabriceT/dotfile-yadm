67f1635d1a6538fc75e91a52e1bddd32d84a86b5
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2025-03-01 19:08:52 +0100

    fish: remove foot term config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,17 +27,3 @@ zoxide init fish | source
 
 # version-fox
 vfox activate fish | source
-
-# foot
-
-function mark_prompt_start --on-event fish_prompt
-    echo -en "\e]133;A\e\\"
-end
-
-function foot_cmd_start --on-event fish_preexec
-  echo -en "\e]133;C\e\\"
-end
-
-function foot_cmd_end --on-event fish_postexec
-  echo -en "\e]133;D\e\\"
-end
