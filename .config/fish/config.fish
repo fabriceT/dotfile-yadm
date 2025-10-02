adcf97837be82eccb078888eddf82e86d1e7de66
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2025-10-02 19:23:04 +0200

    fix(shell): add interactive block

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,20 @@
 fish_add_path ~/go/bin ~/.local/bin ~/.local/share/aquaproj-aqua/bin
 
-# atuin
-#set -gx ATUIN_NOBIND true
-#atuin init fish --disable-up-arrow | source
-#bind \cr _atuin_search
-#bind -M insert \cr _atuin_search
+if status --is-interactive
+    vfox activate fish | source
+    zoxide init fish | source
+    tv init fish | source
 
-# zoxide
-zoxide init fish | source
+    # atuin
+    #set -gx ATUIN_NOBIND true
+    #atuin init fish --disable-up-arrow | source
+    #bind \cr _atuin_search
+    #bind -M insert \cr _atuin_search
 
-# version-fox
-vfox activate fish | source
+    source ~/.config/fish/alias.fish
 
-tv init fish | source
+    bind -M insert ctrl-t tv_smart_autocomplete
+end
 
 set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
 set -gx MOZ_ENABLE_WAYLAND 1
@@ -23,7 +25,3 @@ set -gx AQUA_GLOBAL_CONFIG $HOME/.config/aquaproj-aqua/aqua.yaml
 
 # Fix act https://github.com/nektos/act/issues/303
 set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
-
-source ~/.config/fish/alias.fish
-
-bind -M insert ctrl-t tv_smart_autocomplete
