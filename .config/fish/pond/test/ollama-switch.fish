37d739c31549eb5091d9951437cb7f13189af47a
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2026-03-30 18:55:03 +0200

    feat(ollama): add OLLAMA_API_BASE and OLLAMA_HOST endpoints

--- .config/fish/pond/test/ollama-switch.fish
+++ .config/fish/pond/test/ollama-switch.fish
@@ -2,9 +2,11 @@ function ollama-switch
 
     if set -q OLLAMA_HOST
         echo "Unsetting OLLAMA_HOST"
+        set -xg OLLAMA_API_BASE "http://127.0.0.1:11434"
         set -eg OLLAMA_HOST
     else
-        echo "Setting OLLAMA_HOST"
+        echo "Setting OLLAMA_HOST to diskstation"
         set -xg OLLAMA_HOST "http://192.168.0.133:11434"
+        set -xg OLLAMA_API_BASE "http://192.168.0.133:11434"
     end
 end
