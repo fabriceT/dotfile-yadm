76c99d4a571600dcd8f94b87a0a0e95fbf5e42eb
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2026-05-15 11:06:29 +0200

    fix(fish): add intelli-shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,13 @@
 fish_add_path ~/go/bin ~/.local/bin ~/.local/share/aquaproj-aqua/bin
 
 if status --is-interactive
-    vfox activate fish | source
-    zoxide init fish | source
+    #zoxide init fish | source
+    intelli-shell init fish | source
+    # Hack pour intelli-shell
+    bind --erase --preset -M insert ctrl-space
+    bind --erase ctrl-space
+    bind -M insert ctrl-space _intelli_search
+
     tv init fish | source
     bind -M insert ctrl-t tv_smart_autocomplete
 
@@ -24,5 +29,6 @@ set -gx CHARM_HOST 130.61.241.116
 set -gx AQUA_GLOBAL_CONFIG $HOME/.config/aquaproj-aqua/aqua.yaml
 set -gx LIBVA_DRIVER_NAME i965
 
+set -gx GEMINI_API_KEY AIzaSyCmF_t13rKzwd_vY3qGK1Ay1cXJ0GdMkcI
 # Fix act https://github.com/nektos/act/issues/303
 set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/podman/podman.sock
